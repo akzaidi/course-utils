d973c2336278ee57115e43050e84357df3a17dc8
Ali Zaidi <alikaz.zaidi@gmail.com>
2016-09-14 15:08:38 -0400

    add sh for r materials

---
+++ r-materials.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Download course materials and run user directories
+
+wget http://alizaidi.blob.core.windows.net/training/nyctaxidata.zip
+mkdir /datadrive/data
+chmod -R 0777 /datadrive/data
+unzip nyctaxidata.zip -d /datadrive/
+unzip datadrive/ZillowNeighborhoods-NY.zip
+
+# create users
+
+chmod 755 ./create-users.sh
+./create-users.sh
+
+# download github repo for course materials
+
+git clone https://github.com/akzaidi/Cognizant-R.git /datadrive/
+cp /datadrive/Cognizant-R/MRS\ for\ SAS\ Users.ipynb /notebooks/
+
+# share notebook to all users
+
+chmod 755 ./distribute_materials.csh
+csh ./distribute_materials.csh /notebooks
